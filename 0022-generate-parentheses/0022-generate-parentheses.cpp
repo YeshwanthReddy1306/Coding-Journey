43151f177240648a82ee5f10137628d9fcd40709
YeshwanthReddy1306 <reddyyeshwanth691@gmail.com>
2026-02-17 13:16:29 +0530

    Time: 2 ms (81.8%), Space: 15.7 MB (39.94%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void backtrack(vector<string>& result, string current, int open, int close, int n) {
+        // If valid combination formed
+        if (current.length() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        // Add '(' if we still have some left
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        // Add ')' if it won't make invalid string
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+};
