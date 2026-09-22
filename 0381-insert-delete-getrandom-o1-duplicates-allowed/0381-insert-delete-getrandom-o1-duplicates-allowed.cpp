5796fcb78de04b00bc769624c52ab0adbc5b5bda
Yeshwanth Reddy <reddyyeshwanth691@gmail.com>
2026-09-22 15:33:49 +0530

    Time: 53 ms (45.92%), Space: 114.6 MB (51.6%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -12,45 +12,33 @@ public:
     RandomizedCollection() {}
     
     bool insert(int val) {
-        // Return true if the value is not present in the collection
         bool isNew = valToIndices.find(val) == valToIndices.end() || valToIndices[val].empty();
         
-        // Add the new index to the set for this value
         valToIndices[val].insert(nums.size());
-        // Append the value to the array
         nums.push_back(val);
         
         return isNew;
     }
     
     bool remove(int val) {
-        // If the value doesn't exist, return false
         if (valToIndices.find(val) == valToIndices.end() || valToIndices[val].empty()) {
             return false;
         }
         
-        // Get one of the indices where this value is stored
         int idxToRemove = *valToIndices[val].begin();
         
-        // Remove that index from the value's set
         valToIndices[val].erase(idxToRemove);
         
-        // Get information about the last element in the vector
         int lastIdx = nums.size() - 1;
         int lastElement = nums.back();
-        
-        // If the element to remove is not already the last element, swap them
+    
         if (idxToRemove != lastIdx) {
             nums[idxToRemove] = lastElement;
-            // Update the index set of the last element to reflect its new position
             valToIndices[lastElement].erase(lastIdx);
             valToIndices[lastElement].insert(idxToRemove);
         }
-        
-        // Remove the target element from the back of the vector
         nums.pop_back();
         
-        // Optional cleanup: remove the key from the map if it has no more indices
         if (valToIndices[val].empty()) {
             valToIndices.erase(val);
         }
